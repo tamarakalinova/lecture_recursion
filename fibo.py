6948dd630574df5ad4b2c6498c3152847f5ecf99
Tamara Kalinova <253281@vutbr.cz>
2024-04-22 09:19:04 +0200

    Vypracovany kod

--- fibo.py
+++ fibo.py
@@ -1,6 +1,17 @@
+def recursive_nth_fibo(n):
+    if n <= 1:
+        return n
+    else:
+        return recursive_nth_fibo(n - 1) + recursive_nth_fibo(n - 2)
 
 def main():
-    pass
+    n = int(input("Zadej počet prvků Fibonacciho posloupnosti: "))
+    if n <= 0:
+        print("Zadejte celé kladné číslo!")
+        return
+
+    fibonacci_seq = [recursive_nth_fibo(i) for i in range(n)]
+    print(f"Prvních {n} prvků Fibonacciho posloupnosti je: {fibonacci_seq}")
 
 
 if __name__ == "__main__":
